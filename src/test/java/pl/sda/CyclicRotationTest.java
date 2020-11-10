4ea78d0902dfd6e358dff4ca0439b81c04b06d8c
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-10 19:26:49 +0100

    PrimeDigitRangeCheckerTest

--- src/test/java/pl/sda/CyclicRotationTest.java
+++ src/test/java/pl/sda/CyclicRotationTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -24,7 +26,10 @@ class CyclicRotationTest {
 
     private static Stream<Arguments> prepareInitData() {
         return Stream.of(
-                Arguments.arguments(new ArrayList<>(), 1, new ArrayList<>())
+                Arguments.arguments(new ArrayList<>(), 1, new ArrayList<>()),
+                Arguments.arguments(Arrays.asList(1), 1, Arrays.asList(1)),
+                Arguments.arguments(Arrays.asList(1,2,3), 1, Arrays.asList(3,1,2)),
+                Arguments.arguments(Arrays.asList(3,10,13,15,16), 3, Arrays.asList(13,15,16,3,10))
         );
     }
 
