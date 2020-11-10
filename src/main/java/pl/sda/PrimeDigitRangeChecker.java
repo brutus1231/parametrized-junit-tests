2bd542842676d34fef5a72a003a71285924bec40
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-10 18:56:59 +0100

    PrimeDigitRangeChecker

--- src/main/java/pl/sda/PrimeDigitRangeChecker.java
+++ src/main/java/pl/sda/PrimeDigitRangeChecker.java
@@ -3,6 +3,29 @@ package pl.sda;
 public class PrimeDigitRangeChecker {
 
     public boolean check(Integer a, Integer b) {
-        throw new IllegalStateException();
+        if (a > b) {
+            throw new IllegalStateException();
+        }
+        if (b == 1) {
+            return false;
+        }
+
+        for (int i = a; i <= b; i++) {
+            if (isPrime(i)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isPrime(Integer x) {
+        int counter = 1;
+        for (int i = 2; i <= x; i++) {
+            if (x % i == 0) {
+                counter++;
+            }
+        }
+        return counter == 2 ? true : false;
     }
 }
