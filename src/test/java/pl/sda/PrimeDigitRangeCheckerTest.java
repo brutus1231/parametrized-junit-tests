938c48ff0998b24b4dd05bf7ec277ec7a77316f5
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-10 19:07:59 +0100

    PrimeDigitRangeCheckerTest

--- src/test/java/pl/sda/PrimeDigitRangeCheckerTest.java
+++ src/test/java/pl/sda/PrimeDigitRangeCheckerTest.java
@@ -2,7 +2,11 @@ package pl.sda;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class PrimeDigitRangeCheckerTest {
 
@@ -15,5 +19,29 @@ class PrimeDigitRangeCheckerTest {
         boolean checkResult = checker.check(a, b);
         //then
         Assertions.assertEquals(checkResult, expectedResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("prepareInputData")
+    void shouldCheckIsPrimeOccurredWithMethodSource(Integer a, Integer b, Boolean expectedResult) {
+        //given
+        PrimeDigitRangeChecker checker = new PrimeDigitRangeChecker();
+        //when
+        boolean checkResult = checker.check(a, b);
+        //then
+        Assertions.assertEquals(checkResult, expectedResult);
+    }
+
+    private static Stream<Arguments> prepareInputData(){
+        return Stream.of(
+                Arguments.arguments(10,20, true),
+                Arguments.arguments(-10,-1, false),
+                Arguments.arguments(100,200, true),
+                Arguments.arguments(13,13, true),
+                Arguments.arguments(13,14, true),
+                Arguments.arguments(100,100, false),
+                Arguments.arguments(-10,2, true),
+                Arguments.arguments(-20,1, false)
+        )
     }
 }
