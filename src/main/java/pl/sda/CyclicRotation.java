1b0ab381aa6cb72a2359d0db5ea82d42be5a07be
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-10 19:18:14 +0100

    PrimeDigitRangeCheckerTest

---
+++ src/main/java/pl/sda/CyclicRotation.java
@@ -0,0 +1,10 @@
+package pl.sda;
+
+import java.util.List;
+
+public class CyclicRotation {
+
+    public List<Integer> rotate(List<Integer> digits, Integer shift) {
+        throw new IllegalStateException();
+    }
+}
